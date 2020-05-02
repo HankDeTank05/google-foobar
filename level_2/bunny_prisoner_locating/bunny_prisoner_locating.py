9ca5e404d908d10a0b0fab923dc1da727689fa52
HankDeTank05 <henryhankholman@gmail.com>
2020-05-02 17:07:39 -0500

    changed bunny_prisoner_locating.py after crashing my computer with a test

--- level_2/bunny_prisoner_locating/bunny_prisoner_locating.py
+++ level_2/bunny_prisoner_locating/bunny_prisoner_locating.py
@@ -19,13 +19,13 @@ def generate_cells(x_coord, y_coord):
     y_delta = 1
 
     for row in range(y_coord):
-        print('row {}'.format(row))
+        # print('row {}'.format(row))
         cells.append([y_cell_number])
         x_cell_number = y_cell_number
-        starting_x_delta = row+2
+        starting_x_delta = row + 2
         x_delta = starting_x_delta
 
-        for col in range(x_coord+1):
+        for col in range(x_coord + 1):
             x_cell_number += x_delta
             cells[row].append(x_cell_number)
             x_delta += 1
@@ -38,13 +38,45 @@ def generate_cells(x_coord, y_coord):
 
 
 def solution(x, y):
-    cells = generate_cells(x, y)
-    print(x-1)
-    print(y-1)
-    return cells[y-1][x-1]
+    cells = []
+    x_coord = x
+    y_coord = y
+
+    x_cell_number = 1
+    x_delta = 2
+    starting_x_delta = 2
+    sxd_delta = 1
+
+    y_cell_number = 1
+    y_delta = 1
+
+    for row in range(y_coord):
+        # print('row {}'.format(row))
+        cells.append([y_cell_number])
+        x_cell_number = y_cell_number
+        starting_x_delta = row + 2
+        x_delta = starting_x_delta
+
+        for col in range(x_coord + 1):
+            x_cell_number += x_delta
+            cells[row].append(x_cell_number)
+            x_delta += 1
+
+        y_cell_number += y_delta
+        y_delta += 1
+        print(cells[row])
+    # print(x-1)
+    # print(y-1)
+    return str(cells[y - 1][x - 1])
 
 
+print(solution(1, 1))
+print(solution(2, 3))
+print(solution(3, 2))
 print(solution(5, 10))
+#print(solution(100000, 100000))
+print(solution(100,100))
+print(solution(200,200))
 
 '''generated_cell_list = generate_cells(8, 8)
 for y in range(len(generated_cell_list)):
